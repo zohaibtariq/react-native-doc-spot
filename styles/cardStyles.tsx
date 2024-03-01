9938f5f6bda92e0898315d21f1910519339e0b73
Zohaib Tariq <zohaibtariq@users.noreply.github.com>
2024-03-02 02:24:32 +0500

    cards styling done need to work on equal padding on body container so spacing must look equal on screen boundaries

--- styles/cardStyles.tsx
+++ styles/cardStyles.tsx
@@ -1,6 +1,4 @@
-import {StyleSheet} from 'react-native';
-
-import {Dimensions} from 'react-native';
+import {Dimensions, StyleSheet} from 'react-native';
 
 const screenWidth = Dimensions.get('window').width;
 const width = screenWidth * 0.96;
